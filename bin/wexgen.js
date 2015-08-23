8575b5277c10890b1539c4d34b08feb67601317f
hews <p@hews.co>
2015-08-22 23:07:28 -0700

    squash

--- bin/wexgen.js
+++ bin/wexgen.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 // Import language modules
 var path = require('path'),
     fs   = require('fs-extra');
@@ -5,10 +7,13 @@ var path = require('path'),
 // Import NPM modules
 var jsonfile = require('jsonfile'),
     ejs      = require('ejs'),
-    debug    = require('debug'),
+    debug    = require('debug')('debug'),
+    colors   = require('colors'),
+    program  = require('commander'),
     AppPath  = require('application-resolved-path');
 
 // Import local modules
+var pkg = require('../package');
 
 /***************************
  * Define global variables.
@@ -20,14 +25,17 @@ process.env.DEBUG = '*';
 AppPath.root = path.join(__dirname, '..');
 
 // Define paths.
+
 var __staticsDir   = new AppPath('data/statics',   {app: true}),
     __templatesDir = new AppPath('data/templates', {app: true}),
     __insertsDir   = new AppPath('data/inserts',   {app: true}),
+    manifestFile   = new AppPath('data/manifest.json', {app: true}),
+    remotesFile    = new AppPath('data/remotes.json',  {app: true}),
     statics        = __staticsDir.children(),
     templates      = __templatesDir.children(),
     inserts        = __insertsDir.children(),
     __outputDir,
-    __name;
+    __appname;
 
 // set output path to generate documents
 if ((typeof __outputDir === 'undefined') || NODE_DEBUG) {
@@ -40,8 +48,114 @@ if ((typeof __outputDir === 'undefined') || NODE_DEBUG) {
 }
 
 // set default application name
-if ((typeof __name ==='undefined') || NODE_DEBUG)
-  __name = 'example-app';
+__appname = 'example-app';
+
+/***************************************
+ * Handle options and load the manifest.
+ ***************************************/
+
+// TODO (PJ): update this to include...
+/*
+ * - Example Resources (either in HTML or JSON versions)
+ *     1.  Widget (model/controller/views/seed)
+ *     2.  User w/ Authentication
+ *
+ * --app [basic | full | api | secure-api]
+ *     - basic (SUPER SIMPLE)
+ *     - (default) full: ()
+ *     - api:        (mongo, json version widget scaffold)
+ *     - secure-api: (mongo, web-tokens, json version user & widget scaffold)
+ * --security [local | token | oauth]
+ *
+ */
+
+// TODO (PJ): Defaults for wex are:
+/*
+ * - EJS Templates
+ * - .gitignore
+ * - session support
+ * - no DB
+ * - no security
+ * - no testing
+ * - no client libs
+ * - no GA favicon!
+ */
+
+// parse options
+program
+  .version(pkg.version)
+  .usage('<appname> [options]')
+  .description('Wexgen is an Express application generator that follows WDI conventions.')
+  .arguments('<appname> [options]')
+  .action(function(name) {
+    __appname = name;
+  })
+
+program.on('--help', function(){
+  console.log(
+`  Example:
+
+    ${"$ wexgen new-app-name".green}
+  `);
+});
+
+program
+  // .option('-h, --help',    'print usage information')
+  // .option('-V, --version', 'print the version number')
+  .option('-v, --verbose', 'print verbosely (not implemented)')
+
+  .option('-C, --use-comments', 'add full explanatory comments (not implemented)')
+  .option('-F, --use-force',    'overwrite a non-empty app directory (not implemented)')
+
+  .option('-i, --include-favicon',  'include the GA Gear favicon')
+  .option('-m, --include-mongo',    'include MongoDB and Mongoose')
+  .option('-p, --include-procfile', 'include a Procfile for Heroku')
+  .option('-t, --include-tests',    'include Mocha/Chai BDD testing')
+
+  .option('-$, --include-jquery',    'include client library jQuery')
+  .option('-_, --include-lodash',    'include client library LoDash')
+  .option('-b, --include-bootstrap', 'include client library Bootstrap')
+
+program
+  .command('generate model [Model]')
+  .alias('g model')
+  .description('creates a basic Mongoose model & seed file')
+  .action(function(modelName){
+    program.includeMongo = true
+  });
+
+program
+  .command('generate routes [Resource]')
+  .alias('g routes')
+  .description('creates a basic, RESTful controller')
+
+program
+  .command('generate scaffold [Resource]')
+  .alias('g scaffold')
+  .description('creates a model, controller, and views for a resource ')
+
+program.parse(process.argv);
+
+// console.log(process.argv)
+// if (!process.argv.slice(2).length) {
+//   program.help();
+// }
+
+// console.log(program.usage())
+
+// load manifest json
+var manifest = jsonfile.readFileSync(manifestFile.abs);
+
+var flagged,
+    structs;
+
+// load necessary optional code structures
+for (var option in manifest.options) {
+  flagged = program[option] === true;
+  structs = flagged ? manifest.options[option] : []
+
+  console.log(option + ':', flagged, flagged ? structs : '');
+}
 
 /***********************************
  * Define document parsing helpers.
@@ -66,8 +180,6 @@ var isEjs = function(filename) {
  * Generate static documents.
  *****************************/
 
-// console.log(statics[2].rel);
-
 statics.forEach(function(inputAppPath) {
   var rawFilename   = inputAppPath.filename,
       convertedPath = convertToPath(rawFilename),
@@ -76,7 +188,7 @@ statics.forEach(function(inputAppPath) {
 
   outputPath = path.resolve(
     __outputDir.abs,
-    __name,
+    __appname,
     convertedPath
   );
 
@@ -86,7 +198,7 @@ statics.forEach(function(inputAppPath) {
     if (isEjs(rawFilename)) {
       content = ejs.render(content, {
         app: {
-          name: __name
+          name: __appname
         }
       });
     }
