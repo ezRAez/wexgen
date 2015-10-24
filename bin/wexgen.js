ca1fe35b0575567ed952b3dc0747254f36be456a
hews <p@hews.co>
2015-10-23 22:30:09 -0400

    generating statcis from manifest

--- bin/wexgen.js
+++ bin/wexgen.js
@@ -31,33 +31,71 @@ var remotesFile  = new AppPath('data/remotes.json',  {app: true}),
     insertsDir   = new AppPath('data/inserts',   {app: true}),
     appname;
 
+function createIndenter(indents) {
+  return function(str) {
+    var ind = "";
+    for (var i = 0; i < indents; i++) {
+      ind += " ";
+    };
+    return ind + str;
+  };
+}
 
+function logger() {
+  var text      = arguments[0] || "",
+      indents   = arguments[1] || 0,
+      indent    = createIndenter(indents),
+      maxLines  = arguments[2] || 10,
+      lines     = text.split("\n"),
+      numLines  = lines.length;
+
+  if (numLines > maxLines) {
+    lines = lines.slice(0, maxLines);
+    lines.push(
+      "... (" + numLines + " lines total)");
+  }
+
+  text = lines.map(indent).join("\n");
+
+  console.log(text);
+  return text;
+}
 
 // set default application name
 appname = 'example-app';
 
 /******************************************************
- * Handle CLI input and generates an app description. *
+ * Handle CLI input and generate an app description. *
  ******************************************************/
 
+var definitions = function(app, type) {
+  return app.includes.filter(function(include) {
+    return include.type === type;
+  }).map(function(include) {
+    return include.definition;
+  });
+};
 
 cli(program, appname, process.env.DEBUG) || process.exit();
 
-var app = cli.generatedApp();
+var app     = cli.generatedApp();
+    statics = definitions(app, 'static');
 
 util.inspect(app);
-
-process.exit();
 
 /***********************************
  * Define document parsing helpers.
  ***********************************/
 
-var convertToPath = function(filename) {
+var convertToFilenamePath = function(filename) {
   convertedPath = filename.replace(/\.ejs$/, '');
   convertedPath = convertedPath.replace(/_/, '.');
   convertedPath = convertedPath.replace(/-/, '/');
   return convertedPath;
+};
+
+var addDirectoryToFilenamePath = function(dir, filename) {
+  return dir + '/' + filename;
 }
 
 var isDirectory = function(givenPath) {
@@ -66,37 +104,51 @@ var isDirectory = function(givenPath) {
 
 var isEjs = function(filename) {
   return /\.ejs$/.test(filename);
-}
+};
+
+var readStaticContent = function(inputPath) {
+  content = fs.readFileSync(inputPath, 'utf-8');
+  if (isEjs(path.basename(inputPath))) {
+    content = ejs.render(content, {app: app});
+  }
+  return content;
+};
+
+console.log('');
+console.log('Beginning app creation: ...');
+console.log('');
 
 /*****************************
  * Generate static documents.
  *****************************/
 
-statics.forEach(function(inputAppPath) {
-  var rawFilename   = inputAppPath.filename,
-      convertedPath = convertToPath(rawFilename),
-      outputPath,
+console.log('Generating static documents:');
+
+statics.forEach(function(rawFilename) {
+  var inputFile  = convertToFilenamePath(rawFilename),
+      inputPath  = new AppPath(addDirectoryToFilenamePath(staticsDir, rawFilename), {app: false}),
+      outputFile = app.name + "/" + inputFile,
+      outputPath = path.resolve(outputFile),
       content;
 
-  outputPath = path.resolve(
-    outputDir.abs,
-    appname,
-    convertedPath
-  );
+  // outputPath = path.resolve(
+  //   //app.outputDir.abs //,
+  //   // app.name,
+  //   // filename
+  // );
 
-  if (!isDirectory(inputAppPath.abs)) {
-    content = fs.readFileSync(inputAppPath.abs, 'utf-8');
+  // console.log()
 
-    if (isEjs(rawFilename)) {
-      content = ejs.render(content, {
-        app: {
-          name: appname
-        }
-      });
-    }
-
+  // if (!isDirectory(inputPath)) {
+    console.log('  - writing static file: ' + colors.red(outputFile)
+                /*, '(' + colors.yellow(inputPath.app) + ' -> ' + colors.green(outputFile) + ')'*/);
+    content = readStaticContent(inputPath.abs);
+    logger("==============================", 4)
+    logger(content, 4)
+    logger("==============================", 4)
+    logger("")
     fs.outputFileSync(outputPath, content);
-  }
+  // }
 });
 
 /*******************************
